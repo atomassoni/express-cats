22677f488b197d0c884e76b9d4e7ed272d72e71d
atomassoni <annetomassoni@gmail.com>
2016-05-12 21:23:10 -0500

    disallowed duplicate cats

--- server/routes/felines.js
+++ server/routes/felines.js
@@ -4,13 +4,28 @@ var path = require('path');
 
 var cats = ['Kris', 'Luna', 'Rubio'];
 
+
+
 router.get('/', function(req, res) {
   res.send(cats);
 });
 
 router.post('/', function(req, res) {
-  cats.push(req.body.catName);
+  var match = false;
+  var cat = req.body.catName;
+  cats.forEach(function(item,index,arr) {
+
+
+    if (cat.toLowerCase()==item.toLowerCase()) {
+      match = true;
+      res.send(false);
+    }
+  });
+  if (match==false) {
+  cats.push(cat);
   res.send(true);
+  }
+
 });
 
 
